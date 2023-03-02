73955af687db4e53fafdff5d1380346e040b1f7a
Henry_DelGlitch <CodingWHenry@gmail.com>
2023-03-02 17:27:22 -0500

    Fetches news from Tech Crunch. A loader appears when waiting for data. Cards are clickable. Cards have animation while hovering.

---
+++ src/Compontents/Feed/Feed.js
@@ -0,0 +1,58 @@
+import React from 'react'
+import axios from 'axios'
+import Loader from '../Loader/Loader'
+import Message from './../Message/Message'
+import Post from '../Post/Post'
+import './Feed.css'
+
+export default class Feed extends React.Component {
+    state = {
+        pictureInfo: [],
+        loading: true
+    }
+
+    componentDidMount() {
+        axios(`https://techcrunch.com/wp-json/wp/v2/posts?per_page=20&context=embed`)
+            .then(res => {
+                const pictureInfo = res.data
+                const newState = { ...this.state }
+                newState.pictureInfo = pictureInfo
+                newState.loading = false
+                this.setState(newState)
+            })
+            .catch((err) => {
+                <Message>{err}</Message>
+            })
+    }
+
+    render() {
+        return (
+            <>
+                <div id='company'>
+                    <h1>Veridic News</h1>
+                    <p>Brought to you by News Article API Tech Crunch</p>
+                </div>
+                <div className='feed'>
+                    {
+                        this.state.loading ?
+
+                            <Loader /> :
+
+                            this.state.pictureInfo.map(info => (
+                                <Post
+                                    key={info.id}
+                                    copyright={info['parselyMeta']['parsely-author']}
+                                    date={info.date}
+                                    explanation={info.excerpt.rendered.replace(/(<\/?p>)|(\[[^\]]*\])/gi, "")}
+                                    hdurl={info.jetpack_featured_media_url}
+                                    canonical_url={info.canonical_url}
+                                    id={info.id}
+                                    title={info.title.rendered}
+                                />
+                            ))
+                    }
+                </div>
+            </>
+        )
+    }
+}
---
+++ src/Compontents/Post/Post.js
@@ -0,0 +1,18 @@
+import React from 'react'
+// import Like from '../Like/Like'
+import './Post.css'
+
+const Post = (props) => {
+    return (
+        <a rel="noreferrer" href={props.canonical_url} className='post' id={props.id} target="_blank">
+            <div>
+                <h2 className='post-title'>{props.title}</h2>
+                <p className='post-author'><strong>{props.copyright ? props.copyright : 'Author'} - {new Date(props.date).toLocaleDateString("en-US")}</strong></p>
+                <img className='picture' src={props.hdurl} alt="Image provided by Tech Crunch" />
+                <p className='post-desc'>{props.explanation}</p>
+            </div>
+        </a>
+    )
+}
+
+export default Post
